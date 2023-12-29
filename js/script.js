c1359dffaaab65ef347a7e992268d8f20b7b8321
DiasArdiansyah <153570005+DiasArdiansyah@users.noreply.github.com>
2023-12-29 01:41:05 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// init Isotope
+var $grid = $('.collection-list').isotope({
+    // options
+});
+// filter items on button click
+$('.filter-button-group').on('click', 'button', function () {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
+    $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns() {
+    filterBtns.each(function () {
+        $(this).removeClass('active-filter-btn');
+    });
+}
+
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product => {
+    product.onclick = () => {
+        preveiwContainer.style.display = 'flex';
+        let name = product.getAttribute('data-name');
+        previewBox.forEach(preview => {
+            let target = preview.getAttribute('data-target');
+            if (name == target) {
+                preview.classList.add('active');
+            }
+        });
+    };
+});
+
+previewBox.forEach(close => {
+    close.querySelector('.fa-times').onclick = () => {
+        close.classList.remove('active');
+        preveiwContainer.style.display = 'none';
+    };
+});
